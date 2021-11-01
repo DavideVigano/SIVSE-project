fc7fe8fc0477ee98650ed1f9d0e841275bfd47c0
DavideVigano <davide.vigano2@studenti.unimi.it>
2021-11-01 19:12:29 +0100

    Primo commit

---
+++ SIVSE/src/main/java/it/progetto/ingeneriaDelSoftware/java/SIVSE/App.java
@@ -0,0 +1,13 @@
+package it.progetto.ingeneriaDelSoftware.java.SIVSE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
